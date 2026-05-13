7d807e55f74cfd93e17263da42eec5af9873ae0f
Roque Valledor <roquevalledor@gmail.com>
2026-05-13 11:16:34 +0200

    added necessary changes

--- app/src/main/java/com/example/platformapp/di/RepositoryModule.kt
+++ app/src/main/java/com/example/platformapp/di/RepositoryModule.kt
@@ -1,7 +1,11 @@
 package com.example.platformapp.di
 
 import com.example.platformapp.data.repository.AuthRepositoryImpl
+import com.example.platformapp.data.repository.StorageRepositoryImpl
+import com.example.platformapp.data.repository.UserRepositoryImpl
 import com.example.platformapp.domain.repository.AuthRepository
+import com.example.platformapp.domain.repository.StorageRepository
+import com.example.platformapp.domain.repository.UserRepository
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
